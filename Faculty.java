9b3e30917b7ad17aa5f51b8457f6f495ac202e42
Dom <dominicbdcwalters@gmail.com>
2024-10-20 13:48:09 -0700

    Added Personnel and Faculty Classes
    Personnel contains all the basic and additional information attributes on each employee. Faculty inherits from Personnel and contains attributes that a faculty member would have

---
+++ Faculty.java
@@ -0,0 +1,7 @@
+public class Faculty extends Personnel {
+    //Attributes
+    private String status;
+    private String sabbatical;
+    private String coursesTeaching;
+
+}
---
+++ Personnel.java
@@ -0,0 +1,16 @@
+public class Personnel 
+{
+    //Attributes
+    private String employee_id;
+    private String first_name;
+    private String last_name;
+    private String sex;
+    private String email_address;
+    private String department;
+    private String role;
+    private String join_year;
+    private String bio;
+    private String school_web_link;
+    private String volunteer_activities;
+    private String on_leave;
+}
