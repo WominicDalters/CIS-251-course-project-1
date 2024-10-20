27475a6c57e7495fa0ae340d1bb8cf5f51b517c1
Dom <dominicbdcwalters@gmail.com>
2024-10-20 15:00:57 -0700

    Added constructors
    Added constructors for both the Faculty and the Personnel Classes. Faculty's constructor inherits Personnel's constructor.

--- Faculty.java
+++ Faculty.java
@@ -1,7 +1,23 @@
+
+
 public class Faculty extends Personnel {
     //Attributes
     private boolean status;
     private boolean sabbatical;
     private int courses_teaching;
 
+    //methods
+    public Faculty(String id, String f_name, String l_name, String s, 
+                   String email, String dep, String r, int join_y,
+                   String biography, String s_w_link, String v_activities,
+                   boolean leave, boolean stat, boolean sabbat, int courses)
+    {
+        super(id, f_name, l_name, s, email, dep, r, join_y, biography, 
+              s_w_link, v_activities, leave);
+
+        this.status = stat;
+        this.sabbatical = sabbat;
+        this.courses_teaching = courses;
+
+    }
 }
--- Personnel.java
+++ Personnel.java
@@ -13,4 +13,24 @@ public class Personnel
     private String school_web_link;
     private String volunteer_activities;
     private boolean on_leave;
+
+    //Contructor
+    public Personnel(String id, String f_name, String l_name, String s, 
+                     String email, String dep, String r, int join_y,
+                     String biography, String s_w_link, String v_activities,
+                     boolean leave)
+    {
+        this.employee_id = id;
+        this.first_name = f_name;
+        this.last_name = l_name;
+        this.sex = s;
+        this.email_address = email;
+        this.department = dep;
+        this.role = r;
+        this.join_year = join_y;
+        this.bio = biography;
+        this.school_web_link = s_w_link;
+        this.volunteer_activities = v_activities;
+        this.on_leave = leave;
+    }
 }
